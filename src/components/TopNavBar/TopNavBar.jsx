286b81103e62c25fe37975d879e51361ad22c6de
tonnyvijayan <tonnyvijayan@gmail.com>
2024-06-07 22:06:12 +0530

    feat: added dropdown menu for categories && icon badges

--- src/components/TopNavBar/TopNavBar.jsx
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -5,21 +5,22 @@ import cart from "./assets/cart.svg";
 import user from "./assets/user.svg";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import { useState } from "react";
+import { useProductContext } from "../../hooks/useProductContext";
 
 export const TopNavBar = () => {
+  const { state } = useProductContext();
   const { authState } = useAuth();
+  const [toogleMenu, setToogleMenu] = useState(false);
+  const [toogleCategories, setToogleCategories] = useState(false);
 
-  // const axiosPrivate = useAxiosPrivate();
+  const menuHandler = () => {
+    setToogleMenu((prev) => !prev);
+  };
 
-  // const refreshCheckButtonHanlder = async () => {
-  //   console.log("button clicked");
-  //   try {
-  //     const response = await axiosPrivate.get("/user/checkrefresh");
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const categoriesDropDownHandler = () => {
+    setToogleCategories((prev) => !prev);
+  };
   return (
     <div className="Top-Nav-Bar-Container">
       <div className="brand-logo-container">
@@ -29,21 +30,73 @@ export const TopNavBar = () => {
 
         <strong>PlantMart</strong>
       </div>
-      {JSON.stringify(authState)}
+      {/* {JSON.stringify(authState)} */}
       {/* {JSON.stringify(state.wishlistItems)} */}
 
       {/*  */}
       <div className="user-detail-container">
-        <Link to="/cart">
-          <img src={cart} alt="brand-logo" className="brand-logo" />
-        </Link>
-        <Link to="/wishlist">
-          <img src={wishlist} alt="brand-logo" className="brand-logo" />
-        </Link>
-        <Link to="/">
-          <img src={user} alt="brand-logo" className="brand-logo" />
-        </Link>
+        <div className="icon-badge-container">
+          {state.cartItems.length > 0 ? (
+            <span className="icon-badge-number">{state.cartItems.length}</span>
+          ) : (
+            ""
+          )}
+          <Link to="/cart">
+            <img src={cart} alt="brand-logo" className="brand-logo" />
+          </Link>
+        </div>
+        <div className="icon-badge-container">
+          {state.wishlistItems.length > 0 && authState ? (
+            <span className="icon-badge-number">
+              {state.wishlistItems.length}
+            </span>
+          ) : (
+            ""
+          )}
+
+          <Link to="/wishlist">
+            <img src={wishlist} alt="brand-logo" className="brand-logo" />
+          </Link>
+        </div>
+
+        <div>
+          <div
+            className="menu-div-container"
+            style={{
+              display: toogleMenu ? "block" : "none",
+            }}
+          >
+            <div>
+              {/* <a href="">Categories</a> */}
+              <span onClick={categoriesDropDownHandler}>Categories</span>
+              <div
+                className="categories-list"
+                style={{ display: toogleCategories ? "flex" : "none" }}
+              >
+                <a href="">Outdoor</a>
+                <a href="">Indoor</a>
+                <a href="">Succulents</a>
+                <a href="">Hanging</a>
+              </div>
+            </div>
+            <div>
+              <a href="">Filters</a>
+            </div>
+            <div>
+              <Link to="/login">Login</Link>
+            </div>
+          </div>
+
+          <img
+            src={user}
+            alt="brand-logo"
+            className="brand-logo"
+            onClick={menuHandler}
+          ></img>
+        </div>
       </div>
     </div>
   );
 };
+
+//
